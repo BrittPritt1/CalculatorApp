a1081b9b9f395567f5acf6beae5146c61523ffd1
Britt Breugelmans <breugelmans.britt@gmail.com>
2026-03-14 16:44:37 +0100

    Calculator app - eerste commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("A math operation: ");
+char opr = Console.ReadLine()[0];
+
+Console.WriteLine("First number: ");
+float first = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Second number: ");
+float second = float.Parse(Console.ReadLine());
+
+float result = 0;
+
+switch (opr)
+{
+    case '+' :
+        result = first + second;
+        break;
+
+    case '-' :
+        result = first - second;
+        break;
+
+    case '*' : 
+        result = first * second;
+        break; 
+
+    case '/': 
+        result = first / second;
+        break;
+}
+
+Console.WriteLine($"The result of '{first} {opr} {second}' is: {result}");
